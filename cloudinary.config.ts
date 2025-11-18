0468f2a572019936897fb2aadaddbe04320cda51
ZannGit <ahmad_fauzan_33rpl@student.smktelkom-mlg.sch.id>
2025-11-18 14:54:28 +0700

    Cloudinary

---
+++ cloudinary.config.ts
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from "cloudinary";
+cloudinary.config({
+    cloud_name: "dvn1qilpr",
+    api_key: "934933436169414",
+    api_secret: "BoSH9fwahAYG4Y73eGKkL2hGNOM",
+});
+export default cloudinary;
